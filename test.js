df7f34b1caef6d5f8a4a49371701262801320945
Dakshina Scott <dakshina.scott@gmail.com>
2017-07-05 14:33:31 +0100

    Adds test to check input is unchanged by addtodo Relates #10

--- test.js
+++ test.js
@@ -4,7 +4,30 @@ var logic = require('./logic');
 /* test for add to do function */
 
 test('Add to do', function(t) {
-const actual = logic.addTodo([], {});
-  t.ok (Array.isArray(actual), "check return value is an array");
+  const actual = logic.addTodo([], {});
+  t.ok (Array.isArray(actual), "Return value is an array");
+  t.end();
+});
+
+test('Add to do', function(t) {
+  const original = [
+    {
+      id: 0,
+      description: 'smash avocados',
+      done: true,
+    },
+    {
+      id: 1,
+      description: 'make coffee',
+      done: false,
+    },
+  ];
+  const newTodo = {
+    id: 2,
+    description: 'make more coffee',
+    done: false,
+  };
+  const newList = logic.addTodo(original, newTodo);
+  t.deepEqual(original, newTodo, "Input is unchanged");
   t.end();
 });
