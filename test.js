6bd3375e298345d76c4bfbc365493b5582d61675
Morkeltry <morkeltry@mailfence.com>
2017-07-05 14:26:31 +0100

    two tests added on markTodo

--- test.js
+++ test.js
@@ -25,13 +25,26 @@ const theconstantTodo=
 test('Does it leave input arguments unchanged?', function(t) {
 
 let ourmutableToDo = theconstantTodo.map (x=>x);    //this may be in logic.js
-
-t.equal (thisuniqueresult, functionDoneTo (ourmutableToDo));
-t.equal (theconstantTodo,ourmutableToDo);
-
+let theNewTodo = [];
 
     // should leave the input argument todos unchanged
-    // in the new todo array, all elements will remain unchanged except the one with id: idToMark
+functionDoneTo (ourmutableToDo);
+t.equal (theconstantTodo,ourmutableToDo);
+}
+
+
+test('Does it leave input arguments unchanged?', function(t) {
+
+let ourmutableToDo = theconstantTodo.map (x=>x);    //this may be in logic.js
+let theNewTodo = [];
+
+// in the new todo array, all elements will remain unchanged except the one with id: idToMark
+functionDoneTo (ourmutableToDo);
+t.equal (expectedresult,ourmutableToDo);
+
+}
+
+
     // this element will have its done value toggled
     // hint: array.map
 
