1b8a496a9adc6e841a22f65e19b5d7d76872accb
rachaelcodes <rachaelcodes@users.noreply.github.com>
2017-07-06 12:46:08 +0100

    added a mark button in DOM

--- dom.js
+++ dom.js
@@ -25,10 +25,17 @@
       var newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
     });
+    deleteButtonNode.classList.add('todo__deletebutton');
     todoNode.appendChild(deleteButtonNode);
 
     // add markTodo button
-
+    var markButtonNode = document.createElement('button');
+    markButtonNode.addEventListener('click', function(event) {
+      var newState = todoFunctions.markTodo(state, todo.id);
+      update(newState);
+    });
+    markButtonNode.classList.add('todo__markbutton');
+    todoNode.appendChild(markButtonNode);
     // add classes for css
 
     return todoNode;
